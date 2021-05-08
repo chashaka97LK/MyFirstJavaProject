e0b7d5b838c5cdbf8df9b411b9bababd4a4af124
chashaka97LK <83854076+chashaka97LK@users.noreply.github.com>
2021-05-08 10:27:35 +0530

    1st upload

---
+++ A.java
@@ -0,0 +1,6 @@
+public class A 
+{
+    static public void main(String[] args){
+        System.out.println("I Love Java");
+    }
+}
---
+++ Ilovejava.java
@@ -0,0 +1,6 @@
+public class Ilovejava 
+{
+    static public void main(String[] args){
+        System.out.println("I Love Java");
+    }
+}
